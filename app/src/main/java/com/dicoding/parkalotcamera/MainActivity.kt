14277b01102e8c5ac7c90da59c5cd4cec04bc403
KhaironDaffa <dkhairon@gmail.com>
2021-06-01 19:07:50 +0700

    Initial commit

---
+++ app/src/main/java/com/dicoding/parkalotcamera/MainActivity.kt
@@ -0,0 +1,110 @@
+package com.dicoding.parkalotcamera
+
+import android.app.Activity
+import android.content.ActivityNotFoundException
+import android.content.Intent
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Environment
+import android.os.Environment.getExternalStoragePublicDirectory
+import android.provider.MediaStore
+import androidx.core.content.FileProvider
+import com.dicoding.parkalotcamera.api.ApiConfig
+import com.dicoding.parkalotcamera.databinding.ActivityMainBinding
+import com.dicoding.parkalotcamera.response.UploadResponse
+import com.dicoding.parkalotcamera.utils.getFileName
+import com.dicoding.parkalotcamera.utils.snackbar
+import okhttp3.MediaType
+import okhttp3.MediaType.Companion.toMediaTypeOrNull
+import okhttp3.MultipartBody
+import okhttp3.RequestBody
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import java.io.File
+import java.io.FileInputStream
+import java.io.FileOutputStream
+import java.io.IOException
+import java.text.SimpleDateFormat
+import java.util.*
+
+@Suppress("DEPRECATION")
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private var selectedImageUri: Uri? = null
+    private var nameFile = "ParkaLot "
+
+    companion object {
+        const val REQUEST_CODE_PICK_IMAGE = 101
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.selectImg.setOnClickListener {
+            openImageChooser()
+        }
+
+        binding.upload.setOnClickListener {
+            uploadImage()
+        }
+    }
+
+    private fun openImageChooser() {
+        Intent(Intent.ACTION_PICK).also {
+            it.type = "image/*"
+            val mimeTypes = arrayOf("image/jpeg", "image/png")
+            it.putExtra(Intent.EXTRA_MIME_TYPES, mimeTypes)
+            startActivityForResult(it, REQUEST_CODE_PICK_IMAGE)
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == Activity.RESULT_OK) {
+            when (requestCode) {
+                REQUEST_CODE_PICK_IMAGE -> {
+                    selectedImageUri = data?.data
+                    binding.imgView.setImageURI(selectedImageUri)
+                }
+            }
+        }
+    }
+
+    private fun uploadImage() {
+        if (selectedImageUri == null) {
+            binding.mainRoot.snackbar("Select an Image First")
+            return
+        }
+
+        val parcelFileDescriptor = contentResolver.openFileDescriptor(selectedImageUri!!, "r", null) ?: return
+
+        val inputStream = FileInputStream(parcelFileDescriptor.fileDescriptor)
+        val file = File(cacheDir, contentResolver.getFileName(selectedImageUri!!))
+        val outputStream = FileOutputStream(file)
+        val timeStamp = SimpleDateFormat("EEE, d MMM yyyy - HH:mm:ss", Locale.getDefault()).format(Date())
+        inputStream.copyTo(outputStream)
+
+        val body = UploadRequest(file, "image")
+        val client =
+            ApiConfig.getApiService().uploadImage(MultipartBody.Part.createFormData("image", file.name, body),
+            RequestBody.create("multipart/form-data".toMediaTypeOrNull(), nameFile + timeStamp))
+
+        client.enqueue(object : Callback<UploadResponse> {
+            override fun onFailure(call: Call<UploadResponse>, t: Throwable) {
+                binding.mainRoot.snackbar(t.message!!)
+            }
+
+            override fun onResponse(call: Call<UploadResponse>, response: Response<UploadResponse>) {
+                response.body()?.let {
+                    binding.mainRoot.snackbar(it.message)
+                }
+            }
+        })
+
+    }
+}
---
+++ app/src/main/java/com/dicoding/parkalotcamera/api/ApiService.kt
@@ -0,0 +1,20 @@
+package com.dicoding.parkalotcamera.api
+
+import com.dicoding.parkalotcamera.response.UploadResponse
+import okhttp3.MultipartBody
+import okhttp3.RequestBody
+import retrofit2.Call
+import retrofit2.http.Multipart
+import retrofit2.http.POST
+import retrofit2.http.Part
+
+interface ApiService {
+
+    @Multipart
+    @POST("Api.php?apicall=uploads")
+    fun uploadImage(
+        @Part image: MultipartBody.Part,
+        @Part("desc") desc: RequestBody
+    ): Call<UploadResponse>
+
+}
